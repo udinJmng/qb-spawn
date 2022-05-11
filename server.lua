550247dadfa3525750adaf5a2832c2028945ac82
Aditya Taher <86789381+udinJmng@users.noreply.github.com>
2022-05-11 10:32:18 +0700

    Update server.lua

--- server.lua
+++ server.lua
@@ -1,6 +1,13 @@
-local QBCore = exports['qb-core']:GetCoreObject()
+ESX = nil
 
-QBCore.Functions.CreateCallback('qb-spawn:server:getOwnedHouses', function(source, cb, cid)
+Citizen.CreateThread(function()
+  while ESX == nil do
+    TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
+    Citizen.Wait(30)-- Saniye Bekletme
+  end
+end)
+
+ESX.RegisterServerCallback('qb-spawn:server:getOwnedHouses', function(source, cb, cid)
     if cid ~= nil then
         local houses = MySQL.Sync.fetchAll('SELECT * FROM player_houses WHERE citizenid = ?', {cid})
         if houses[1] ~= nil then
